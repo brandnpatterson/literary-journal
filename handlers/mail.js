36cc5c29810abf0f81730a821c774053d483ece1
Brandon Patterson <brandnpatterson@gmail.com>
2018-02-26 22:32:32 -0600

    mail host

--- handlers/mail.js
+++ handlers/mail.js
@@ -5,7 +5,8 @@ const htmlToText = require('html-to-text');
 const promisify = require('es6-promisify');
 
 const transport = nodemailer.createTransport({
-  service: 'gmail',
+  host: process.env.MAIL_HOST,
+  port: process.env.MAIL_PORT,
   auth: {
     user: process.env.MAIL_USER,
     pass: process.env.MAIL_PASS,
