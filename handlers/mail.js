69e630bc797ceb4ef12756fe14c4fd237677edd9
Brandon Patterson <brandnpatterson@gmail.com>
2018-02-26 22:50:30 -0600

    local email working

--- handlers/mail.js
+++ handlers/mail.js
@@ -5,7 +5,7 @@ const htmlToText = require('html-to-text');
 const promisify = require('es6-promisify');
 
 const transport = nodemailer.createTransport({
-  service: 'gmail',
+  service: process.env.MAIL_HOST,
   auth: {
     user: process.env.MAIL_USER,
     pass: process.env.MAIL_PASS,
